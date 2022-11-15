37ba06c94fee7808cd3025ae73b483ccec2586d3
Muhammed Ashik <116175002+mhdashikofficial@users.noreply.github.com>
2022-11-15 15:35:08 +0530

    Update nmap-result.sh

--- nmap-result.sh
+++ nmap-result.sh
@@ -1,9 +1,12 @@
+banner NMAP RESULT TAKER
+echo maded by ashikthehacker.com
 echo Enter your target ip:  
 read ip
 echo enter output file name:
 read name
+cd ..
 cd Desktop
-mkdir results 
+mkdir -p results 
 cd results
 touch $name.txt
 nmap -oN /home/kali/Desktop/results/$name.txt -A $ip
